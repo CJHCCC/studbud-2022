ae07aa1188c4b4d1ecba7d5316c50dcdfe5a6c26
Chun-Han Chen <648694159@qq.com>
2022-05-15 03:13:12 +1000

    Timer

---
+++ public/script.js
@@ -0,0 +1,38 @@
+let timef = 0;
+let times = 0;
+let timem = 0;
+let zt_ks = false;
+let fg = Array();
+//timer
+setInterval(() => {
+    if (zt_ks)
+        timem++;
+    if (timem == 60) {
+        timem = 0;
+        timef++;
+        if (timef == 60) {
+            timef = 0;
+            times++;
+        }
+    }
+    document.getElementById("s").innerHTML = times;
+    document.getElementById("f").innerHTML = timef;
+    document.getElementById("m").innerHTML = timem;
+}, 1000);
+//timer
+function bulle(i) {
+    switch (i) {
+        case 0:
+            zt_ks = false;
+            break;
+        case 1:
+            zt_ks = true;
+            break;
+        case 2:
+            zt_ks = false;
+            timef = 0;
+            times = 0;
+            timem = 0;
+            break;
+    }
+}
