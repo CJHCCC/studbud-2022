e8ed1db3342299014c1443b539a20aedf991b5ea
CJHCCC <100389137+CJHCCC@users.noreply.github.com>
2022-06-04 14:55:06 +1000

    Update calendar.js

--- public/calendar.js
+++ public/calendar.js
@@ -1,184 +1,189 @@
-(function(){
-    /*
-     * 用于记录日期，显示的时候，根据dateObj中的日期的年月显示
-     */
-    var dateObj = (function(){
-      var _date = new Date();    // 默认为当前系统时间
-      return {
-        getDate : function(){
-          return _date;
-        },
-        setDate : function(date) {
-          _date = date;
-        }
-      };
-    })();
-  
-    // 设置calendar div中的html部分
-    renderHtml();
-    // 表格中显示日期
-    showCalendarData();
-    // 绑定事件
-    bindEvent();
-  
-    /**
-     * 渲染html结构
-     */
-    function renderHtml() {
-      var calendar = document.getElementById("calendar");
-      var titleBox = document.createElement("div");  // 标题盒子 设置上一月 下一月 标题
-      var bodyBox = document.createElement("div");  // 表格区 显示数据
-  
-      // 设置标题盒子中的html
-      titleBox.className = 'calendar-title-box';
-      titleBox.innerHTML = "<span class='prev-month' id='prevMonth'></span>" +
-        "<span class='calendar-title' id='calendarTitle'></span>" +
-        "<span class='add-task' id='addTask'></span>"+
-        "<span class='next-month' id='nextMonth'></span>" ;
-        
-      calendar.appendChild(titleBox);    // 添加到calendar div中
-  
-      // 设置表格区的html结构
-      bodyBox.className = 'calendar-body-box';
-      var _headHtml = "<tr>" + 
-                "<th>Su</th>" +
-                "<th>Mo</th>" +
-                "<th>Tu</th>" +
-                "<th>We</th>" +
-                "<th>Th</th>" +
-                "<th>Fr</th>" +
-                "<th>Sa</th>" +
-              "</tr>";
-      var _bodyHtml = "";
-  
-      // 一个月最多31天，所以一个月最多占6行表格
-      for(var i = 0; i < 6; i++) {  
-        _bodyHtml += "<tr>" +
-                "<td></td>" +
-                "<td></td>" +
-                "<td></td>" +
-                "<td></td>" +
-                "<td></td>" +
-                "<td></td>" +
-                "<td></td>" +
-              "</tr>";
-      }
-      bodyBox.innerHTML = "<table id='calendarTable' class='calendar-table'>" +
-                _headHtml + _bodyHtml +
-                "</table>";
-      // 添加到calendar div中
-      calendar.appendChild(bodyBox);
-    }
-  
-    /**
-     * 表格中显示数据，并设置类名
-     */
-    function showCalendarData() {
-      var _year = dateObj.getDate().getFullYear();
-      var _month = dateObj.getDate().getMonth() + 1;
-      var _dateStr = getDateStr(dateObj.getDate());
-  
-      // 设置顶部标题栏中的 年、月信息
-      var calendarTitle = document.getElementById("calendarTitle");
-      var titleStr = _dateStr.substr(0, 4) + "Y." + _dateStr.substr(4,2) + "M.";
-      calendarTitle.innerText = titleStr;
-  
-      // 设置表格中的日期数据
-      var _table = document.getElementById("calendarTable");
-      var _tds = _table.getElementsByTagName("td");
-      var _firstDay = new Date(_year, _month - 1, 1);  // 当前月第一天
-      for(var i = 0; i < _tds.length; i++) {
-        var _thisDay = new Date(_year, _month - 1, i + 1 - _firstDay.getDay());
-        var _thisDayStr = getDateStr(_thisDay);
-        _tds[i].innerText = _thisDay.getDate();
-        //_tds[i].data = _thisDayStr;
-        _tds[i].setAttribute('data', _thisDayStr);
-        if(_thisDayStr == getDateStr(new Date())) {    // 当前天
-          _tds[i].className = 'currentDay';
-        }else if(_thisDayStr.substr(0, 6) == getDateStr(_firstDay).substr(0, 6)) {
-          _tds[i].className = 'currentMonth';  // 当前月
-        }else {    // 其他月
-          _tds[i].className = 'otherMonth';
-        }
-      }
-    }
-  
-    /**
-     * 绑定上个月下个月事件
-     */
-    function bindEvent() {
-      var prevMonth = document.getElementById("prevMonth");
-      var nextMonth = document.getElementById("nextMonth");
-      var addTask = document.getElementById("addTask");
-      addEvent(prevMonth, 'click', toPrevMonth);
-      addEvent(nextMonth, 'click', toNextMonth);
-      addEvent(addTask, 'click', toAddTask );
+(function () {
+  // Used to record the date. When it is displayed, it is displayed according to the year and month of the date in dateObj.
 
-      var table = document.getElementById("calendarTable");
-      var tds = table.getElementsByTagName('td');
-      for(var i = 0; i < tds.length; i++) {
-          addEvent(tds[i], 'click', (e)=>{
-            for(var j = 0; j < tds.length; j++){
-              tds[j].classList.remove('clickMonth');
-            }
-            e.target.classList.add('clickMonth');
-            console.log(e.target.getAttribute('data'));
-          });
+  var dateObj = (function () {
+    var _date = new Date();    // default systeam time 
+    return {
+      getDate: function () {
+        return _date;
+      },
+      setDate: function (date) {
+        _date = date;
       }
-    }
-  
-    /**
-     * 绑定事件
-     */
-    function addEvent(dom, eType, func) {
-      if(dom.addEventListener) {  // DOM 2.0
-        dom.addEventListener(eType, function(e){
-          func(e);
-        });
-      } else if(dom.attachEvent){  // IE5+
-        dom.attachEvent('on' + eType, function(e){
-          func(e);
-        });
-      } else {  // DOM 0
-        dom['on' + eType] = function(e) {
-          func(e);
-        }
-      }
-    }
-  
-    /**
-     * 点击上个月图标触发
-     */
-    function toPrevMonth() {
-      var date = dateObj.getDate();
-      dateObj.setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
-      showCalendarData();
-    }
-  
-    /**
-     * 点击下个月图标触发
-     */
-    function toNextMonth() {
-      var date = dateObj.getDate();
-      dateObj.setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
-      showCalendarData();
-    }
-
-    function toAddTask(){
-        
-    }
-  
-    /**
-     * 日期转化为字符串， 4位年+2位月+2位日
-     */
-    function getDateStr(date) {
-      var _year = date.getFullYear();
-      var _month = date.getMonth() + 1;    // 月从0开始计数
-      var _d = date.getDate();
-      
-      _month = (_month > 9) ? ("" + _month) : ("0" + _month);
-      _d = (_d > 9) ? ("" + _d) : ("0" + _d);
-      return _year + _month + _d;
-    }
+    };
   })();
+
+  renderHtml();
+
+  showCalendarData();
+
+  bindEvent();
+
+  // render Html
+  function renderHtml() {
+    var calendar = document.getElementById("calendar");
+    var titleBox = document.createElement("div");
+    var bodyBox = document.createElement("div");
+
+    // Give it a classname
+    titleBox.className = 'calendar-title-box';
+    titleBox.innerHTML = "<span class='prev-month' id='preMonth'></span>" +
+      "<span class='calendar-title' id='calendarTitle'></span>" +
+      "<span class='next-month' id='nextMonth'></span>";
+    // add calendar to div
+    calendar.appendChild(titleBox);
+
+    // set day names monday to sunday
+    bodyBox.className = 'calendar-body-box';
+    var _headHtml = "<tr>" +
+      "<th>Su</th>" +
+      "<th>Mo</th>" +
+      "<th>Tu</th>" +
+      "<th>We</th>" +
+      "<th>Th</th>" +
+      "<th>Fr</th>" +
+      "<th>Sa</th>" +
+      "</tr>";
+    var _bodyHtml = "";
+
+    // Logic 31days need 6 line
+    for (var i = 0; i < 6; i++) {
+      _bodyHtml += "<tr>" +
+        "<td></td>" +
+        "<td></td>" +
+        "<td></td>" +
+        "<td></td>" +
+        "<td></td>" +
+        "<td></td>" +
+        "<td></td>" +
+        "</tr>";
+    }
+    bodyBox.innerHTML = "<table id='calendarTable' class='calendar-table'>" +
+      _headHtml + _bodyHtml +
+      "</table>";
+    calendar.appendChild(bodyBox);
+  }
+
+
+  // display date in form
+  function showCalendarData() {
+    var _year = dateObj.getDate().getFullYear();
+    var _month = dateObj.getDate().getMonth() + 1;
+    var _dateStr = getDateStr(dateObj.getDate());
+
+    // Set year and month
+    var calendarTitle = document.getElementById("calendarTitle");
+    var titleStr = _dateStr.substr(0, 4) + "Y." + _dateStr.substr(4, 2) + "M.";
+    calendarTitle.innerText = titleStr;
+
+    // Set days
+    var tablee = document.getElementById("calendarTable");
+    var tdss = tablee.getElementsByTagName("td");
+    var firstDay = new Date(_year, _month - 1, 1);
+    for (var i = 0; i < tdss.length; i++) {
+      var _thisDay = new Date(_year, _month - 1, i + 1 - firstDay.getDay());
+      var _thisDayStr = getDateStr(_thisDay);
+      tdss[i].innerText = _thisDay.getDate();
+      tdss[i].data = _thisDayStr;
+      tdss[i].setAttribute('data', _thisDayStr);
+
+      taskListArray = document.getElementById("tasklist");
+      console.log(taskListArray.getDay);
+      if (taskListArray.dueDate == tdss[i].data) {
+        tdss[i].style.borderBottom = "1px dashed red";
+        renderHtml();
+      }
+      // today
+      if (_thisDayStr == getDateStr(new Date())) {
+        tdss[i].className = 'currentDay';
+      }
+      // current month
+      else if (_thisDayStr.substr(0, 6) == getDateStr(firstDay).substr(0, 6)) {
+        tdss[i].className = 'currentMonth';
+      }
+      // other month
+      else {
+        tdss[i].className = 'otherMonth';
+      }
+    }
+  }
+
+
+  /**
+   * to click last and next month
+   */
+  function bindEvent() {
+    var preMonth = document.getElementById("preMonth");
+    var nextMonth = document.getElementById("nextMonth");
+    addEvent(preMonth, 'click', toPrevMonth);
+    addEvent(nextMonth, 'click', toNextMonth);
+
+    var table = document.getElementById("calendarTable");
+    var tds = table.getElementsByTagName('td');
+    for (var i = 0; i < tds.length; i++) {
+      addEvent(tds[i], 'click', (e) => {
+        for (var j = 0; j < tds.length; j++) {
+          tds[j].classList.remove('clickMonth');
+        }
+        e.target.classList.add('clickMonth');
+        console.log(e.target.getAttribute('data'));
+      });
+    }
+  }
+
+  /**
+   * Add Event
+   */
+  function addEvent(dom, eType, func) {
+    if (dom.addEventListener) {
+      dom.addEventListener(eType, function (e) {
+        func(e);
+      });
+    } else if (dom.attachEvent) {
+      dom.attachEvent('on' + eType, function (e) {
+        func(e);
+      });
+    } else {
+      dom['on' + eType] = function (e) {
+        func(e);
+      }
+    }
+  }
+
+  /**
+   * last month button
+   */
+  function toPrevMonth() {
+    var date = dateObj.getDate();
+    dateObj.setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
+    showCalendarData();
+  }
+
+  /**
+   * Next month button
+   */
+  function toNextMonth() {
+    var date = dateObj.getDate();
+    dateObj.setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
+    showCalendarData();
+  }
+
+  function toAddTask() {
+
+  }
+
+  /**
+   * render date
+   */
+  function getDateStr(date) {
+    var _year = date.getFullYear();
+    // strat from 0
+    var _month = date.getMonth() + 1;
+    var _d = date.getDate();
+
+    _month = (_month > 9) ? ("" + _month) : ("0" + _month);
+    _d = (_d > 9) ? ("" + _d) : ("0" + _d);
+    return _year + _month + _d;
+  }
+})();
 
